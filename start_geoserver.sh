8daef5b8c9ab59885f6c67693b25de5b0ee9e820
slush62 <steve@lushfamily.com>
2020-06-01 10:45:09 -0500

    run detached

--- start_geoserver.sh
+++ start_geoserver.sh
@@ -1,5 +1,5 @@
-# docker run -it -d --name=geoserver --network=geoswarm \
-docker run -it --rm --name=geoserver --network=geoswarm \
+# docker run -it --rm --name=geoserver --network=geoswarm \
+docker run -it -d --restart=always --name=geoserver --network=geoswarm \
 -e GEOSERVER_DATA_DIR=/geoserver_data/data \
 -e GEOSERVER_DATA_ROOT=/geoserver_data \
 -v /var/run/docker.sock:/var/run/docker.sock \
